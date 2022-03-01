f24ef5e977ac44a083f8c958b8bd507b5b6c8580
Denis <majestycrew@gmail.com>
2022-03-01 20:43:40 +0300

    full

--- HW2.2/ViewController.swift
+++ HW2.2/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var viewOutlet: UIView!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewOutlet.layer.cornerRadius = viewOutlet.frame.height / 10
     }
-
-
+    
+    private func changeColor() {
+        viewOutlet.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    
+    @IBAction func redLadelAction() {
+        redLabel.text = String(format: "%.1f", redSlider.value)
+        greenLabel.text = String(format: "%.1f", greenSlider.value)
+        blueLabel.text = String(format: "%.1f", blueSlider.value)
+        changeColor()
+    }
 }
+
 
